5d7700df0eacd108a49a295d2dda296005e2520d
Saurabh Dudhe <130128590+Sdedits@users.noreply.github.com>
2024-05-30 14:57:19 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -96,11 +96,9 @@ async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="che
         else:
             return True
     except UserNotParticipant:
-        text="""**Click the  𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐭𝐨 𝐣𝐨𝐢𝐧 and then click 𝐓𝐫𝐲 𝐀𝐠𝐚𝐢𝐧 and you will get the File...😁
+        text="""**Click on the Request to join and Join the channel to Get Lecture/Notes/DPP ❤️
 
-ശ്രദ്ധിക്കുക
-
-താഴെ ഉള്ള ജോയിൻ ലിങ്കിൽ ക്ലിക്ക് ചെയ്തു 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐭𝐨 𝐣𝐨𝐢𝐧 ക്ലിക്ക് ചെയ്ത് കഴിഞ്ഞ് 𝐓𝐫𝐲 𝐀𝐠𝐚𝐢𝐧 ക്ലിക്ക് ചെയ്‌താൽ നിങ്ങൾക് സിനിമ ലഭിക്കുന്നതാണ്...😁**"""
+Join Below Channel To Get Lecture 😁**"""
 
         buttons = [
             [
@@ -109,8 +107,8 @@ async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="che
             [
                 InlineKeyboardButton(" 🔄 Try Again 🔄 ", callback_data=f"{mode}#{file_id}")
             ],
-            [   InlineKeyboardButton("Update", url="https://t.me/VJ_Botz"),
-                InlineKeyboardButton("YouTube", url="https://youtube.com/@Tech_VJ")
+            [   InlineKeyboardButton("Main Channel", url="https://t.me/teamvishwas"),
+                InlineKeyboardButton("Back-Up Channel", url="https://t.me/teamvishwasofficial")
             ]
         ]
         
